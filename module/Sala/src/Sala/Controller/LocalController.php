c80113158a0873bdde65e48e479e4066a19bf68e
ProgUESB <chicobrandao.si@gmail.com>
2013-09-26 11:23:30 -0300

    Revert "Revert "local porra""
    This reverts commit 66cf29ed8f69c38456196c9128fd661f9a3b14b3.

---
+++ module/Sala/src/Sala/Controller/LocalController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Sala\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel; 
+use Sala\Form\LocalForm;
+use Doctrine\ORM\EntityManager;
+use Sala\Entity\Local;
+
+class LocalController extends AbstractActionController
+{
+    /**
+     * @var Doctrine\ORM\EntityManager
+     */
+    protected $em;
+
+    public function setEntityManager(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+ 
+    /**
+     * Return a EntityManager
+     *
+     * @return Doctrine\ORM\EntityManager
+     */
+    public function getEntityManager()
+    {
+        if (null === $this->em) {
+            $this->em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        }
+        
+        return $this->em;
+    } 
+
+    public function indexAction()
+    {
+        return new ViewModel(array(
+            'locais' => $this->getEntityManager()->getRepository('Sala\Entity\Local')->findAll() 
+        ));
+    }
+
+    public function addAction()
+    {
+        $form = new LocalForm();
+        $form->get('submit')->setAttribute('label', 'Add');
+
+        $request = $this->getRequest();
+        
+        if ($request->isPost()) {
+            $local = new Local();
+            
+            $form->setInputFilter($local->getInputFilter());
+            $form->setData($request->getPost());
+            
+            if ($form->isValid()) { 
+                $local->populate($form->getData()); 
+                
+                $this->getEntityManager()->persist($local);
+                $this->getEntityManager()->flush();
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('local'); 
+            }
+        }
+
+        return array('form' => $form);
+    }
+
+    public function editAction()
+    {
+        $id = (int) $this->getEvent()->getRouteMatch()->getParam('id');
+        
+        if (!$id) {
+            return $this->redirect()->toRoute('local', array('action'=>'add'));
+        } 
+        
+        $local = $this->getEntityManager()->find('Sala\Entity\Local', $id);
+
+        $form = new LocalForm();
+        $form->setBindOnValidate(false);
+        $form->bind($local);
+        $form->get('submit')->setAttribute('label', 'Edit');
+        
+        $request = $this->getRequest();
+        
+        if ($request->isPost()) {
+        
+            $form->setData($request->getPost());
+            
+            if ($form->isValid()) {
+                $form->bindValues();
+                $this->getEntityManager()->flush();
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('local');
+            }
+        }
+
+        return array(
+            'id' => $id,
+            'form' => $form,
+        );
+    }
+
+    public function deleteAction()
+    {
+        $id = (int)$this->getEvent()->getRouteMatch()->getParam('id');
+        
+        if (!$id) {
+            return $this->redirect()->toRoute('local');
+        }
+
+        $request = $this->getRequest();
+        
+        if ($request->isPost()) {
+            $del = $request->getPost('del', 'Não');
+            
+            if ($del == 'Sim') {
+                $id = (int) $request->getPost('id');
+                $local = $this->getEntityManager()->find('Sala\Entity\Local', $id);
+                
+                if ($local) {
+                    $this->getEntityManager()->remove($local);
+                    $this->getEntityManager()->flush();
+                }
+            }
+
+            return $this->redirect()->toRoute('local');
+        }
+
+        return array(
+            'id' => $id,
+            'local' => $this->getEntityManager()->find('Sala\Entity\Local', $id)
+        );
+    }
+}
